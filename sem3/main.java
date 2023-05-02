f0d486bfcdd0d7b5365cb2e0d2cb17a16b3bf479
EliazBid <124061492+EliazBid@users.noreply.github.com>
2023-05-02 14:08:41 +0200

    Add files via upload

---
+++ sem3/main.java
@@ -0,0 +1,10 @@
+import se.kth.iv1350.sem3.EVW.View.*;
+
+public class main {
+    
+    public static void main(String[] args) {
+        RegistryCreator creator = new RegistryCreator();
+        view view = new view();
+        view.startProcess();
+    }
+}
---
+++ sem3/se/kth/iv1350/sem3/EVW/View/view.java
@@ -0,0 +1,6 @@
+package se.kth.iv1350.sem3.EVW.View;
+public class view {
+    public void startProcess() {
+        System.out.println("it works");
+    }
+}
