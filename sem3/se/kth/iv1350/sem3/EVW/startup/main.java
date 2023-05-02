69b7559533abff75771a4c52a19a1807888dffd7
MrBregott <108457182+MrBregott@users.noreply.github.com>
2023-05-02 14:30:23 +0200

    stuff
    stuff

---
+++ sem3/se/kth/iv1350/sem3/EVW/startup/main.java
@@ -0,0 +1,12 @@
+package se.kth.iv1350.sem3.EVW.startup;
+
+import se.kth.iv1350.sem3.EVW.View.*;
+
+public class main {
+    
+    public static void main(String[] args) {
+        RegistryCreator creator = new RegistryCreator();
+        view view = new view();
+        view.startProcess();
+    }
+}
