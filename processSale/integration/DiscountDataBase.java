479dfc8c4e2c303fda9bb7003f02ba5586e0eae0
EliazBid <124061492+EliazBid@users.noreply.github.com>
2023-05-03 14:21:41 +0200

    Update DiscountDataBase.java

--- processSale/integration/DiscountDataBase.java
+++ processSale/integration/DiscountDataBase.java
@@ -2,12 +2,23 @@ package processSale.integration;
 
 public class DiscountDataBase {
 
-	DiscountDataBase DiscountDataBase() {
-		return null;
-	}
+	/**
+     	 * Unknow how this database operates thus no attributes are created. 
+     	 */
 
-	public void fetchDiscount() {
+    	/**
+     	 * Creates an instance of the discount database.
+     	 */
+    	public DiscountDataBase() {
+        
+    	}
 
-	}
+    	/**
+     	 * Fetches the discount from the database.
+     	 * Never actually used in seminar 3 but would be used in a real system.
+     	 */
+    	public void fetchDiscount() {
+        
+    	}
 
 }
