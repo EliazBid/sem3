beedc69a61ca8c45b00cfc9f425833493fb4dc9d
EliazBid <124061492+EliazBid@users.noreply.github.com>
2023-05-03 14:42:41 +0200

    Update CashRegister.java

--- processSale/model/CashRegister.java
+++ processSale/model/CashRegister.java
@@ -1,11 +1,19 @@
 package processSale.model;
 
 public class CashRegister {
-
-	public CashRegister CashRegister() {
-		return null;
+	
+	/**
+	 * Creates an instance of the cash register
+	 */
+	public CashRegister() {
+		
 	}
 
+	/**
+	 * Adds the money given to the cashier to the cash register total
+	 *
+	 * @param toPay The money give by the shopper 
+	 */
 	public void addMoney(CashPayment toPay) {
 
 	}
