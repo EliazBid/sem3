3f21ad532e570a9de4e7fb7447ec6a17cbdacd65
EliazBid <eliaz@kth.se>
2023-05-03 18:19:14 +0200

    update main

--- processSale/view/View.java
+++ processSale/view/View.java
@@ -1,10 +1,12 @@
 package processSale.view;
 
 import processSale.controller.Controller;
+import processSale.model.Amount;
 
 public class View {
 
 	private Controller contr;
+	private Amount amount = new Amount(100.0);
 
 	public View(Controller contr) {
 		this.contr = contr;
@@ -13,6 +15,8 @@ public class View {
 	public void runProgram(){
 		contr.startSale();
 		System.out.println("New sale started!");
+		contr.scanItem(5);
+		contr.endSale();
 		contr.enterAmountPaid(amount);
 	}
 }
