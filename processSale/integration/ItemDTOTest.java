e1990d96292a6870a87ac61e50f3b1a20255e719
undercoverNya <wmj@kth.se>
2023-05-04 22:43:03 +0200

    improved quantity test

--- processSale/integration/ItemDTOTest.java
+++ processSale/integration/ItemDTOTest.java
@@ -19,7 +19,8 @@ public class ItemDTOTest {
         ItemDTO reference = new ItemDTO(1, 1, new Amount(10), "Milk", "A carton of milk", 1.12);
         ItemDTO testItem = new ItemDTO(1, 1, new Amount(10), "Milk", "A carton of milk", 1.12);
         testItem.increaseQuantity();
-        assertNotEquals(reference.getQuantity(),testItem.getQuantity());
+        assertTrue("Did not increase quantity", reference.getQuantity() < testItem.getQuantity());
+        assertTrue("Increased inaccurately", testItem.getQuantity() < reference.getQuantity()+2);
         assertNotNull(testItem);
         assertNotNull(reference);
 
