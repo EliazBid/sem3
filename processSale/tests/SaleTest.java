d567b7b3115a9efcbc582f9b4dd3fba36f93a06a
vfredl <132370749+vfredl@users.noreply.github.com>
2023-05-04 23:42:21 +0200

    Update SaleTest.java

--- processSale/tests/SaleTest.java
+++ processSale/tests/SaleTest.java
@@ -16,30 +16,30 @@ public class SaleTest {
 
         Sale sale = new Sale();
 
-        ItemDTO itemDTO1 = new ItemDTO(2, 1, new Amount(20), "Bread", "A loaf of bread", 1.06);
-        ItemDTO itemDTO2 = new ItemDTO(2, 2, new Amount(20), "Bread", "A loaf of bread", 1.06);
+        ItemDTO itemDTO = new ItemDTO(2, 1, new Amount(20), "Bread", "A loaf of bread", 1.06);
+        ItemDTO anotherItemDTO = new ItemDTO(2, 2, new Amount(20), "Bread", "A loaf of bread", 1.06);
         
         
-        sale.addItem(itemDTO1);
+        sale.addItem(itemDTO);
         assertEquals("wrong number of item(s) added", sale.getItemList().size(), 1);
-        assertEquals("item added to wrong index",sale.getItemList().get(0), itemDTO1);
+        assertEquals("item added to wrong index",sale.getItemList().get(0), itemDTO);
         assertEquals("quantity of item 1 incorrect", sale.getItemList().get(0).getQuantity(), 1);
         
         
-        sale.addItem(itemDTO1);
+        sale.addItem(itemDTO);
         assertEquals("itemList size increased instead of quantity", sale.getItemList().size(), 1);
         assertEquals("quantity of item 1 incorrect", sale.getItemList().get(0).getQuantity(), 2);
         
         
-        sale.addItem(itemDTO2);
+        sale.addItem(anotherItemDTO);
         assertEquals("item added successfully",sale.getItemList().size(),2);
-        assertEquals("item added in wrong index",sale.getItemList().get(0), itemDTO2);
-        assertEquals("item added in wrong index",sale.getItemList().get(1), itemDTO1);
+        assertEquals("item added in wrong index",sale.getItemList().get(0), anotherItemDTO);
+        assertEquals("item added in wrong index",sale.getItemList().get(1), itemDTO);
         assertEquals("quantity of item 2 incorrect", sale.getItemList().get(0).getQuantity(), 2);
         assertEquals("quantity of item 1 incorrect", sale.getItemList().get(1).getQuantity(), 2);
         
         
-        sale.addItem(itemDTO1);
+        sale.addItem(itemDTO);
         assertEquals("itemList size increased instead of quantity", sale.getItemList().size(), 2);
         assertEquals("quantity of item 1 incorrect", sale.getItemList().get(1).getQuantity(), 3);
     }
