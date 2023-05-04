3727c3da3630c232a575e13826f8ca2f39082855
undercoverNya <wmj@kth.se>
2023-05-04 21:09:37 +0200

    Create SaleTest.java

---
+++ processSale/model/SaleTest.java
@@ -0,0 +1,24 @@
+package processSale.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SaleTest {
+
+    @Test
+    public void addItem() {
+    }
+
+    @Test
+    public void applyDiscountToRunningTotal() {
+    }
+
+    @Test
+    public void pay() {
+    }
+
+    @Test
+    public void getReceipt() {
+    }
+}
