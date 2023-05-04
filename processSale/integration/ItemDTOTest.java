90caf227d31cd21a25be1a88b51607567a0faa18
undercoverNya <wmj@kth.se>
2023-05-04 21:00:20 +0200

    test added

---
+++ processSale/integration/ItemDTOTest.java
@@ -0,0 +1,40 @@
+package processSale.integration;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Before;
+import processSale.model.Amount;
+import processSale.integration.ItemDTO;
+
+
+import static org.junit.Assert.*;
+
+public class ItemDTOTest {
+
+
+
+
+    @Test
+    public void increaseQuantityTest() {
+        ItemDTO reference = new ItemDTO(1, 1, new Amount(10), "Milk", "A carton of milk", 1.12);
+        ItemDTO testItem = new ItemDTO(1, 1, new Amount(10), "Milk", "A carton of milk", 1.12);
+        testItem.increaseQuantity();
+        assertNotEquals(reference.getQuantity(),testItem.getQuantity());
+        assertNotNull(testItem);
+        assertNotNull(reference);
+
+    }
+
+    @Test
+    public void getFinalPriceTest() {
+        ItemDTO testItem = new ItemDTO(1, 3, new Amount(10), "Milk", "A carton of milk", 1.12);
+        Amount quantity = new Amount(3);
+        Amount cost = new Amount(10);
+        cost.multiply(quantity);
+        assertEquals(cost,testItem.getFinalPrice());
+
+    }
+
+
+
+}
