fafe74b53245d8ead435bb1b924662a5cb5a57b2
vfredl <132370749+vfredl@users.noreply.github.com>
2023-05-04 21:23:17 +0200

    Update SaleTest.java

--- processSale/model/SaleTest.java
+++ processSale/model/SaleTest.java
@@ -1,21 +1,30 @@
 package processSale.model;
 
 import org.junit.Test;
-
+import java.util.ArrayList;
+import processSale.integration.ItemDTO;
 import static org.junit.Assert.*;
 
 public class SaleTest {
 
+    private ArrayList<ItemDTO> itemLists = new ArrayList<ItemDTO>();
+    private ArrayList<ItemDTO> itemLists2 = new ArrayList<ItemDTO>();
+
     @Test
     public void addItem() {
+        ItemDTO itemDTO = new ItemDTO(1, 1, new Amount(10), "Milk", "A carton of milk", 1.12);
+        itemLists.add(itemDTO);
+        assertTrue(itemLists.contains(itemDTO));
     }
 
     @Test
-    public void applyDiscountToRunningTotal() {
-    }
-
-    @Test
-    public void pay() {
+    public void additem() {
+        ItemDTO itemDTO = new ItemDTO(2, 1, new Amount(20), "Bread", "A loaf of bread", 1.06);
+        ItemDTO itemDTO2 = new ItemDTO(2, 2, new Amount(20), "Bread", "A loaf of bread", 1.06);
+        itemLists2.add(itemDTO);
+        itemLists2.add(itemDTO);
+        System.out.println(itemLists);
+        assertTrue(itemLists.contains(itemDTO2));
     }
 
     @Test
